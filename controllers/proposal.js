f0945b78c12109374588b6d75b3adbd4d2d18f6e
sem-a <aleksey.i99@mail.ru>
2024-12-28 15:19:59 +0300

    текстовое описание API для предложений

---
+++ controllers/proposal.js
@@ -0,0 +1,33 @@
+/**
+ * @route /api/proposal/
+ * @method GET
+ * @desc получить все предложения
+ * @access Public
+ */
+
+
+/**
+ * @route /api/proposal/:id
+ * @method GET
+ * @desc получить предложение по id
+ * @access Public
+ */
+
+
+/**
+ * @route /api/proposal/author
+ * @method GET
+ * @desc получить все предложения конкретного автора
+ * @access Public
+ */
+
+
+/**
+ * @route /api/proposal/vote
+ * @method POST
+ * @desc получить все предложения
+ * @access Public
+ */
+
+
+
---
+++ routes/proposal.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// /api/proposal
+router.post("/",);
+
+// /api/proposal/:id
+router.post("/:id",);
+
+module.exports = router;
